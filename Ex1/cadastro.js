f4c4679d170bfd3545a472e0c3fe2d4f026c1c86
Guilherme Seixas <guilhermeseixasabreu@gmail.com>
2022-12-19 19:41:51 -0300

    ex1

---
+++ Ex1/cadastro.js
@@ -0,0 +1,9 @@
+nome = prompt("bota teu nome")
+sobrenome = prompt("Seu sobrenome")
+campo = prompt("Campo de estudo?")
+ano = prompt("ano de nascimento")
+
+nascimento = 2022 - ano
+
+alert(`Nome: ${nome}\nSobrenome: ${sobrenome}\nCampo de estudo: ${campo}\nIdade: ${nascimento}`)
+
---
+++ Ex1/calculadora.js
@@ -0,0 +1,9 @@
+primeiro = Number(prompt("Insira um numero"))
+segundo = Number(prompt("Insira mais um numero"))
+
+soma = primeiro + segundo
+sub = primeiro - segundo
+multi = primeiro * segundo
+div = primeiro / segundo
+
+alert(`Soma: ${soma}\nSubtracao: ${sub}\nMultiplicacao: ${multi}\nDivisao: ${div}`)
